3344488510a83454946e633279c83980f33b0863
stecala <ste.cala123@gmail.com>
2022-07-19 15:09:20 +0200

    🏠Repo creation

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HeaderApp />
+    <MainApp />
+    <FooterApp />
+  </div>
+</template>
+
+<script>
+import MainApp from './components/mainApp.vue'
+import HeaderApp from './components/headerApp.vue'
+import FooterApp from './components/mainApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainApp,
+    HeaderApp,
+    FooterApp,
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+</style>

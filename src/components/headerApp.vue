20723401c123b3464757320dc3b1df2d5574673d
stecala <ste.cala123@gmail.com>
2022-07-19 15:34:33 +0200

    Feat ⭐ add variables file and complete the header

--- src/components/headerApp.vue
+++ src/components/headerApp.vue
@@ -1,7 +1,12 @@
 <template>
-  <header>
-    
-  </header>
+    <header>
+        <div class="logo-container">
+            <a href="">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
+                alt="spotify logo">
+            </a>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -10,6 +15,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/style/variables.scss";
 
+.logo-container {
+    background-color: $backgroundHeader;
+
+    img {
+        width: 40px;
+        height: 40px;
+        margin: 15px;
+    }
+}
 </style>
--- src/components/mainApp.vue
+++ src/components/mainApp.vue
@@ -10,5 +10,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../assets/style/variables.scss";
 
+main{
+  height: 46.35vh;
+  background-color: $backgroundMain;
+}
 </style>

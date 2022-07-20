e90cf1a949f197e6c49e9b6c6a9cf6225df246d4
stecala <ste.cala123@gmail.com>
2022-07-20 15:17:44 +0200

    Feat ⭐ add authors search but doesn't work now

---
+++ src/components/authorsUser.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <select class="form-select" aria-label="Default select example" v-model="authorSelect"
+            @change="$emit('selectAuthor', authorSelect)">
+            <option v-for="(element, index) in authorsList" :value="element" :key="index">{{ element }}</option>
+            <option value="">All Authors</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            authorSelect: ''
+        }
+    },
+     props : {
+        authorsList : Array
+    },
+}
+</script>
+
+<style>
+</style>
--- src/components/mainApp.vue
+++ src/components/mainApp.vue
@@ -1,17 +1,16 @@
 <template>
   <main class="d-flex justify-content-center align-items-center">
-      <div class="container pt-5" v-if="(loader)">
-        <div class="row">
-            <genresUser class="col-4 m-auto" @select="setUserSelect" :genresList="genresList" />
-        </div>
-        <div class="row justify-content-center">
-            <SingleCard class="col-2 me-3" v-for="(element, index) in filteredCards"  :key="index"
-                :index='index'
-                :element="element" 
-            />
-        </div>
+    <div class="container pt-5" v-if="(loader)">
+      <div class="row">
+        <genresUser class="col-4 m-auto" @select="setUserSelect" :genresList="genresList" />
+        <authorsUser class="col-4 m-auto" @selectAuthor="authorSelect" :authorsList="aurhorsList" />
       </div>
-      <LoaderApp v-if="(!loader)" />
+      <div class="row justify-content-center">
+        <SingleCard class="col-2 me-3" v-for="(element, index) in filteredCards" :key="index" :index='index'
+          :element="element" />
+      </div>
+    </div>
+    <LoaderApp v-if="(!loader)" />
   </main>
 </template>
 
@@ -19,70 +18,83 @@
 import SingleCard from './singleCard.vue'
 import LoaderApp from './loaderApp.vue'
 import genresUser from './genresUser.vue'
+import authorsUser from './authorsUser.vue'
 import axios from 'axios'
 
 export default {
-  components :{
+  components: {
     SingleCard,
     LoaderApp,
     genresUser,
-},
-  data : function(){
+    authorsUser,
+  },
+  data: function () {
     return {
-      cardsList : [],
-      filteredCards : [],
-      loader : false,
-      userSelected : '',
-      genresList : []
+      cardsList: [],
+      filteredCards: [],
+      loader: false,
+      userSelected: '',
+      authorSelected: '',
+      genresList: [],
+      authorsList: [],
     }
   },
-  methods : {
-    getCard(){
+  methods: {
+    getCard() {
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-      .then((result)=> {
-        this.cardsList = result.data.response
-        this.filteredCards= [...this.cardsList]
-        this.initGenres()
-        this.timeOut()
-        this.searchGenre(this.userSelected)
+        .then((result) => {
+          this.cardsList = result.data.response
+          this.filteredCards = [...this.cardsList]
+          this.initGenres()
+          this.initAuthors()
+          this.timeOut()
+          this.searchGenre(this.userSelected)
+          this.searchAuthor(this.authorSelected)
 
-      })
+        })
     },
-    timeOut(){
-      setTimeout(()=>{
+    timeOut() {
+      setTimeout(() => {
         this.loader = true
-      },2000)
+      }, 2000)
     },
-    searchGenre(element){
-        console.log(element)
-        this.filteredCards= [...this.cardsList].filter((artist) => artist.genre.includes(element))  
+    searchGenre(element) {
+      console.log(element)
+      this.filteredCards = [...this.cardsList].filter((artist) => artist.genre.includes(element))
     },
-    setUserSelect(choice){
-        this.userSelected = choice
-        this.searchGenre(this.userSelected)
+    searchAuthor(element) {
+      this.filteredCards = [...this.cardsList].filter((artist) => artist.author.includes(element))
     },
-    initGenres(){
-        for(let i =0 ; i< this.cardsList.length; i++){
-          if(!this.genresList.includes(this.cardsList[i].genre)){
-            this.genresList.push(this.cardsList[i].genre)
-          }
+    setUserSelect(choice) {
+      this.userSelected = choice
+      this.searchGenre(this.userSelected)
+    },
+    initGenres() {
+      for (let i = 0; i < this.cardsList.length; i++) {
+        if (!this.genresList.includes(this.cardsList[i].genre)) {
+          this.genresList.push(this.cardsList[i].genre)
         }
-        
-    }
-  },
-  created(){
-    this.getCard()
-  } 
+      }
+    },
+    initAuthors() {
+      for (let i = 0; i < this.cardsList.length; i++) {
+        if (!this.authorsList.includes(this.cardsList[i].author)) {
+          this.authorsList.push(this.cardsList[i].author)
+        }
+      }
+    },
+    created() {
+      this.getCard()
+    },
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import "../assets/style/variables.scss";
-main{
+
+main {
   height: calc(100vh - 70px);
   background-color: $backgroundMain;
 }
-
-
-
 </style>
